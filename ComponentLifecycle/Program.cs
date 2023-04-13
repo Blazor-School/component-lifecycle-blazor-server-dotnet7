96f57a9f219c7c6a40a61f5fa9f0b5f66adc2ae0
Red Wei <huy.tran@blazorschool.com>
2023-04-13 20:44:12 +0700

    Add project files.

---
+++ ComponentLifecycle/Program.cs
@@ -0,0 +1,19 @@
+using ComponentLifecycle.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<LoggingService>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
